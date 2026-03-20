0dd4b26f9ecd2d528b4da46bd07766c7a4700b6f
Rishit Srivastava <197550397+rishit-exe@users.noreply.github.com>
2026-03-20 22:44:05 +0530

    fix: reduced columns for platinum sponsors from 4 to 3

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -3,7 +3,7 @@ import { apiLinks } from "../../data/apiLinks";
 
 const TIER_CONFIG = {
   Platinum: {
-    cols: "grid-cols-1 sm:grid-cols-2 lg:grid-cols-4",
+    cols: "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3",
     filter: "",
   },
   Gold: {
